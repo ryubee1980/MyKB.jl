c72ae600c689ad6095f6be0288eb4a00ea572a74
ryubee1980 <ryubee@gmail.com>
2022-05-23 14:52:51 +0900

    commit!

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,14 @@ using Test
 
 @testset "MyKB.jl" begin
     # Write your tests here.
+    fn="rdftest.xvg"
+    s=0.052
+    gr=MyKB.in_RDF(fn)
+    MyKB.hr(gr,shift=s)
+    MyKB.comp_GR(fn,shift=s)
+    MyKB.plot_GR(fn,shift=s)
+    MyKB.fit_GR(fn,0.8,1.4,shift=s)
+    MyKB.eval_KB(fn,0.8,1.4,shift=s)
+    MyKB.plot_GR_fit(fn,0.8,1.4,shift=s)
+
 end
