e57098ce848cb7e67a92194cfa778daf2ac1ee92
ryubee1980 <ryubee@gmail.com>
2022-05-24 15:42:06 +0900

    test updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,13 @@ using Test
     # Write your tests here.
     fn="rdftest.xvg"
     s=0.052
+    @test_nowarn gr=MyKB.in_RDF(fn)
     gr=MyKB.in_RDF(fn)
-    MyKB.hr(gr,shift=s)
-    MyKB.comp_GR(fn,shift=s)
-    MyKB.plot_GR(fn,shift=s)
-    MyKB.fit_GR(fn,0.8,1.4,shift=s)
-    MyKB.eval_KB(fn,0.8,1.4,shift=s)
-    MyKB.plot_GR_fit(fn,0.8,1.4,shift=s)
+    @test_nowarn MyKB.hr(gr,shift=s)
+    @test_nowarn MyKB.comp_GR(fn,shift=s)
+    @test_nowarn MyKB.plot_GR(fn,shift=s)
+    @test_nowarn MyKB.fit_GR(fn,0.8,1.4,shift=s)
+    @test_nowarn MyKB.eval_KB(fn,0.8,1.4,shift=s)
+    @test_nowarn MyKB.plot_GR_fit(fn,0.8,1.4,shift=s)
 
 end
