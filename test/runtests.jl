67b3aad218da1f1629e0af71bc899e39b259f90b
ryubee1980 <ryubee@gmail.com>
2024-11-21 10:58:17 +0900

    test updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
     @test_nowarn gr=MyKB.in_RDF(fn)
     gr=MyKB.in_RDF(fn,ave_range=100)
     @test_nowarn MyKB.hr(gr)
-    @test_nowarn MyKB.comp_GR(fn,save_range=100)
+    @test_nowarn MyKB.comp_GR(fn,ave_range=100)
     @test_nowarn MyKB.plot_GR(fn,ave_range=100)
     @test_nowarn MyKB.plot_RGR(fn,ave_range=100)
     @test_nowarn MyKB.fit_GR(fn,0.8,1.4,ave_range=100)
